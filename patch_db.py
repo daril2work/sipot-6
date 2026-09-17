76340716eee6c50755e4e1b13623fba5ea77e533
daril2work <daril2work@gmail.com>
2026-09-17 13:51:29 +0700

    fix: Use relative path for patch_db.py

--- patch_db.py
+++ patch_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def patch_db():
-    conn = sqlite3.connect('d:/sipot/sipot-6-flask/farmasi_lplpo.db')
+    conn = sqlite3.connect('farmasi_lplpo.db')
     try:
         conn.execute('ALTER TABLE user ADD COLUMN telegram_chat_id VARCHAR(50);')
         print("Column telegram_chat_id added successfully.")
